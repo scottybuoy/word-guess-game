7c75102c53ddafd51898a67055ab8dde885223d6
Scott Everett <burningcupidxxx@gmail.com>
2022-01-03 16:30:01 -0800

    set up landing page, cursory styling

---
+++ assets/script.js
@@ -0,0 +1,26 @@
+var startButton = document.querySelector("#startButton");
+var title = document.querySelector("#title");
+var mainCont = document.querySelector("#mainCont");
+var formCont = document.querySelector("#formCont");
+
+
+
+
+
+
+
+
+startButton.addEventListener("click", trans1);
+
+function trans1() {
+    mainCont.removeChild(startButton);
+    mainCont.removeChild(title);
+    formCont.setAttribute("style", "display: flex;");
+    var testP = document.createElement("span");
+    testP.setAttribute("id", "spanTest");
+    mainCont.appendChild(testP);
+    testP.textContent = "okkk";
+
+
+};
+
